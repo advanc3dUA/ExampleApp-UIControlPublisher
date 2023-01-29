51244520c6ae7ca9411b0dc9628bcc1482182473
advanc3dUA <fash1k@gmail.com>
2023-01-29 10:11:39 +0100

    initial commit

---
+++ ExampleApp-UIControlPublisher/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ExampleApp-UIControlPublisher
+//
+//  Created by Yuriy Gudimov on 29.01.2023.
+//
+
+import UIKit
+import Combine
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var r: UISlider!
+    @IBOutlet weak var g: UISlider!
+    @IBOutlet weak var b: UISlider!
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
